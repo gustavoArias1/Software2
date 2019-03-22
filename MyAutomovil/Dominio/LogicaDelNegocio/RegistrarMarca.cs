54454692459c9378387c0c48092512f04aeeb39e
Manuel David Galvis Cardenas <oncecaldasgalviscardenas24@hotmail.com>
2019-03-22 11:42:31 -0500

    Implementacion parcial RegistrarMarca

--- MyAutomovil/Dominio/LogicaDelNegocio/RegistrarMarca.cs
+++ MyAutomovil/Dominio/LogicaDelNegocio/RegistrarMarca.cs
@@ -7,25 +7,62 @@ namespace Dominio.LogicaDelNegocio
 {
     class RegistrarMarca
     {
+        List<Marca> marcas = new List<Marca>();
         public void AdicionarMarca(string nombreMarca,string pais)
         {
-
+            //Acceso base de datos metodo recuperar marcas
+            Marca marca = new Marca(nombreMarca, pais);
+            if (marcas.Contains(marca))
+            {
+                //Exception
+            }
+            else
+            {
+                marcas.Add(marca);
+                //Exception
+            }
         }
 
         public void EliminarMarca(string nombreMarca)
         {
-
+            //Acceso base de datos enviando delete
+            //Exception
         }
 
-        public void ActualizarMarca()
+        public void ActualizarMarca(string nombreMarca, string pais)
         {
-
+            //Acceso base de datos enviando nombreMerca, pais con sentencia update;
         }
-        public  Marca ConsultarMarca(string nombre)
+
+        public Marca ConsultarMarca(string nombre)
         {
+            //Acceso base de datos metodo recuperar marcas
+            Marca marca = null;
+            try
+            {
+                for (int i = 0; i < marcas.Count; i++)
+                {
+                    if (marcas[i].Nombre.Equals(nombre))
+                    {
+                        marca = marcas[i];
+                    }
+                }
+            } catch (Exception ex)
+            {
+                //Exception
+            }
+            if (marca == null)
+            {
+                //Exception
+            }
+            return marca;
+        }
+
+        public List<Marca> RecuperarMarcas()
+        {
+            //Codigo de acceso a la base de datos
             return null;
         }
-
     }
 }
 
