e656411e3d26a44eaca1bd22b101b9d2faca7f70
William <willivasquez98@gmail.com>
2019-03-22 11:41:11 -0500

    Ajustes para caso de uso Registrar Empleado

--- MyAutomovil/Dominio/EntidadesDominio/Administrador.cs
+++ MyAutomovil/Dominio/EntidadesDominio/Administrador.cs
@@ -6,6 +6,7 @@ namespace Dominio.EntidadesDominio
 {
     class Administrador
     {
+        private int codigo;
         private string nombre;
         private string apellido;
         private int cedula;
@@ -13,10 +14,34 @@ namespace Dominio.EntidadesDominio
         private DateTime fechaNacimiento;
         private string correo;
         private string contraseña;
-        private int codigo;
+        private Concesionario concesionario;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Cedula { get => cedula; set => cedula = value; }
+        public string Cargo { get => cargo; set => cargo = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public string Contraseña { get => contraseña; set => contraseña = value; }
+        internal Concesionario Concesionario { get => concesionario; set => concesionario = value; }
 
         public Administrador(string nombre, string apellido, int cedula, string cargo, DateTime fechaNacimiento, 
-            string correo, string contraseña, int codigo)
+            string correo, string contraseña)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.cedula = cedula;
+            this.cargo = cargo;
+            this.fechaNacimiento = fechaNacimiento;
+            this.correo = correo;
+            this.contraseña = contraseña;
+            this.codigo = 0;
+            this.concesionario = BuscarConcesionario(concesionario.NombreConcesionario);
+        }
+
+        public Administrador(string nombre, string apellido, int cedula, string cargo, DateTime fechaNacimiento,
+            string correo, string contraseña,int codigo,Concesionario concesionario)
         {
             this.nombre = nombre;
             this.apellido = apellido;
@@ -26,6 +51,27 @@ namespace Dominio.EntidadesDominio
             this.correo = correo;
             this.contraseña = contraseña;
             this.codigo = codigo;
+            this.concesionario = concesionario;
+        }
+
+
+        private Concesionario BuscarConcesionario(string nombreconcesionario) {
+            Concesionario c = null;
+            
+            /*
+            aqui viene la extraccion de la DB del concesionario
+
+            c = new Concesionario();
+             */ 
+            return null;
+        }
+
+        private int buscarCodigo(int cedula, string nombre) {
+            int codigo = 0;
+            /*
+             Traemos el codigo de la base de datos
+             */
+            return codigo;
         }
     }
 }
--- MyAutomovil/Dominio/EntidadesDominio/Concesionario.cs
+++ MyAutomovil/Dominio/EntidadesDominio/Concesionario.cs
@@ -6,11 +6,13 @@ namespace Dominio.EntidadesDominio
 {
     class Concesionario
     {
+        private int codigo; 
         private string nombreConcesionario;
         private Administrador administrador;
         private string fecha;
         private string telefono;
         private string ciudad;
+        List<Vendedor> vendedores = null;
 
         public Concesionario(string nombreConcesionario, Administrador administrador, string fecha, string telefono, string ciudad)
         {
@@ -20,5 +22,22 @@ namespace Dominio.EntidadesDominio
             this.telefono = telefono;
             this.ciudad = ciudad;
         }
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string NombreConcesionario { get => nombreConcesionario; set => nombreConcesionario = value; }
+        public string Fecha { get => fecha; set => fecha = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Ciudad { get => ciudad; set => ciudad = value; }
+        internal Administrador Administrador { get => administrador; set => administrador = value; }
+
+        private List<Vendedor> RecuperarEmpleados(Concesionario concesionario) {
+            List<Vendedor> vendedoresAux = null;
+            /*
+             Consulta DB
+             */
+            return vendedoresAux;
+        }
+
+
     }
 }
