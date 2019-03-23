6328f755445741d448f44d32cc1d8f6486ac9f8e
William <willivasquez98@gmail.com>
2019-03-22 22:55:09 -0500

    Modificaciones y registrar Cliente

--- MyAutomovil/Dominio/LogicaDelNegocio/RegistrarEmpleado.cs
+++ MyAutomovil/Dominio/LogicaDelNegocio/RegistrarEmpleado.cs
@@ -9,7 +9,8 @@ namespace Dominio.LogicaDelNegocio
     {
         List<Vendedor> vendedores = null;
 
-        public void AdicionarEmpleado(string nombre,string apellido, int cedula,string cargo, DateTime fechanacimiento,string nombreConcesionario)
+        public void AdicionarEmpleado(string nombre,string apellido, int cedula,string cargo, DateTime fechanacimiento,
+            string nombreConcesionario)
         {
             Concesionario concesionario = RecuperarConcesionario(nombreConcesionario);
             concesionario.vendedores = concesionario.RecuperarEmpleados();
@@ -35,7 +36,8 @@ namespace Dominio.LogicaDelNegocio
              eliminar empleado en DB con sentencia delete
              */
         }
-        public void ActualizarEmpleado(string nombre, string apellido, int cedula, string cargo, DateTime fechanacimiento, Concesionario concesionario,string correo,string contraseña)
+        public void ActualizarEmpleado(string nombre, string apellido, int cedula, string cargo, DateTime fechanacimiento,
+            Concesionario concesionario,string correo,string contraseña)
         {
             /*
              actualizar empleado con sentencia update
@@ -47,9 +49,6 @@ namespace Dominio.LogicaDelNegocio
             @vendedoresAux lista que retornaremos con los empleados
              */
             List<Vendedor> vendedoresAux = new List<Vendedor>();
-            /*
-             consulta base de datos 
-             */
 
             Concesionario concesionario = RecuperarConcesionario(nombreConcesionario);
             concesionario.vendedores = concesionario.RecuperarEmpleados();
@@ -57,27 +56,28 @@ namespace Dominio.LogicaDelNegocio
             {
                 for (int i = 0; i < concesionario.vendedores.Count; i++)
                 {
-                    if (concesionario.vendedores[i].Cedula == cedula)
-                    {
-                        vendedoresAux.Add(concesionario.vendedores[i]);
-                    }
-                    else if (concesionario.vendedores[i].Nombre == nombre)
-                    {
-                        vendedoresAux.Add(concesionario.vendedores[i]);
-                    }
-                    else if (concesionario.vendedores[i].Apellido == apellido)
+                    if (concesionario.vendedores[i].Cedula == cedula || (concesionario.vendedores[i].Nombre.Equals(nombre)) ||
+                            (concesionario.vendedores[i].Apellido.Equals(apellido)))
                     {
                         vendedoresAux.Add(concesionario.vendedores[i]);
                     }
                 }
             }
             catch (Exception ex) {
-                //exec
+                //excepcion
             }
             if (concesionario.vendedores.Count == 0) {
                 //excepcion no hay coincidencias
+                return null;
             }
             return vendedoresAux;
+        }
+
+        public List<Vendedor> ConsultarEmpleado(string nombreConcesionario)
+        {
+            Concesionario concesionario = RecuperarConcesionario(nombreConcesionario);
+            concesionario.vendedores = concesionario.RecuperarEmpleados();
+            return concesionario.vendedores;
         }
 
         /*
