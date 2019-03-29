729240e2c57856767e1faf5779e9f201c9cca391
gustavo andres arias loaiza <guandres-94@hotmail.com>
2019-03-29 14:34:45 -0500

    creacion de datos de marca

--- MyAutomovil/Persistencia/DBFake.cs
+++ MyAutomovil/Persistencia/DBFake.cs
@@ -6,7 +6,18 @@ namespace Persistencia
 {
     public class DBFake
     {
-        Marca mc = new Marca("audi","alemania");
         
+        public List<Marca> AgregarMarcas()
+        {
+            List<Marca> listaMarcas = new List<Marca>();
+            Marca m1 = new Marca("audi", "alemania");
+            Marca m2 = new Marca("ford", "Colombia");
+            Marca m3 = new Marca("mazda", "japon");
+            listaMarcas.Add(m1);
+            listaMarcas.Add(m2);
+            listaMarcas.Add(m3);
+            return listaMarcas;
+        }
+
     }
 }
