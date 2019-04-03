0a6d15af2f0565ad44eededb1f746075e30c2e37
gustavo andres arias loaiza <guandres-94@hotmail.com>
2019-04-03 16:00:24 -0500

    creacion de otro constructor en concesionario

--- MyAutomovil/Dominio/EntidadesDominio/Concesionario.cs
+++ MyAutomovil/Dominio/EntidadesDominio/Concesionario.cs
@@ -8,25 +8,35 @@ namespace Dominio.EntidadesDominio
     {
         private int codigo; 
         private string nombreConcesionario;
-        private String nombreAdministrador;
-        private string fecha;
+        private string nombreAdministrador;
+        private string direccion;
         private string telefono;
         private string ciudad;
         public List<Vendedor> vendedores = null;
         public List<Vehiculo> vehiculos = null;
 
-        public Concesionario(string nombreConcesionario, string administrador, string fecha, string telefono, string ciudad)
+        public Concesionario(string nombreConcesionario, string administrador, string direccion, string telefono, string ciudad)
         {
             this.nombreConcesionario = nombreConcesionario;
             this.nombreAdministrador = administrador;
-            this.fecha = fecha;
+            this.direccion = direccion;
             this.telefono = telefono;
             this.ciudad = ciudad;
+        }
+        public Concesionario(int codigo, string nombreConcesionario, string administrador, string direccion, string telefono, string ciudad)
+        {
+            this.nombreConcesionario = nombreConcesionario;
+            this.nombreAdministrador = administrador;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.ciudad = ciudad;
+            this.codigo = codigo;
+            this.vendedores = new List<Vendedor>();
         }
 
         public int Codigo { get => codigo; set => codigo = value; }
         public string NombreConcesionario { get => nombreConcesionario; set => nombreConcesionario = value; }
-        public string Fecha { get => fecha; set => fecha = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
         public string Telefono { get => telefono; set => telefono = value; }
         public string Ciudad { get => ciudad; set => ciudad = value; }
         public string Administrador { get => nombreAdministrador; set => nombreAdministrador = value; }
@@ -41,7 +51,7 @@ namespace Dominio.EntidadesDominio
 
 
          public List<Vendedor> RecuperarVehiculos() {
-            List<Vehiculos> vehiculos = new List<Vehiculo>();
+            List<Vehiculo> vehiculos = new List<Vehiculo>();
             /*
              Consulta DB
              */
