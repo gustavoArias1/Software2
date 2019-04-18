e7e6e86cc3356f96b73eff783f8761b1b2d83da2
Manuel David Galvis Cardenas <mdgcardenas24@gmail.com>
2019-04-17 19:08:23 -0500

    Vista recuperar contraseña

---
+++ MyAutomovil/MVC/Controllers/RecuperarContraseñaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class RecuperarContraseñaController : Controller
+    {
+        // GET: RecuperarContraseña
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RecuperarContraseña()
+        {
+
+        }
+    }
+}
