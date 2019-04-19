c654269592eeacb25d35c7b5d9d9e6d36a61f5aa
Manuel David Galvis Cardenas <mdgcardenas24@gmail.com>
2019-04-18 19:57:58 -0500

    Adicionar marca vista, correccion en demas vistas de nombre de atributos.

---
+++ MyAutomovil/MVC/Controllers/RegistrarMarcaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class RegistrarMarcaController : Controller
+    {
+        // GET: RegistrarMarca
+        public ActionResult AdicionarMarca()
+        {
+            return View();
+        }
+    }
+}
