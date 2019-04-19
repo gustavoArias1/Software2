81943b687edc2083dfc5578d2f67bf6f5a562fbc
Manuel David Galvis Cardenas <mdgcardenas24@gmail.com>
2019-04-18 19:44:13 -0500

    Creacion de vistas de adicionar y actualizar empleado, correccion de titulos en actualizar y adicionar cliente y creacion de titulo en index, adicion de controladora registrarEmpleado

---
+++ MyAutomovil/MVC/Controllers/RegistrarEmpleadoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class RegistrarEmpleadoController : Controller
+    {
+        // GET: RegistrarEmpleado
+        public ActionResult AdicionarEmpleado()
+        {
+            return View();
+        }
+
+        public ActionResult ActualizarEmpleado()
+        {
+            return View();
+        }
+    }
+}
