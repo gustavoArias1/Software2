dc1635f3e9ce8bb30700e52881be98d32c1dbf36
gustavo andres arias loaiza <guandres-94@hotmail.com>
2019-04-26 09:06:17 -0500

    Vista AdicionarConcesionario

---
+++ MyAutomovil/ClassLibrary1/AdicionarConcesionarioViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fachada
+{
+    public class AdicionarConcesionarioViewModel
+    {
+        public string NombreConcesionario { get; set; }
+        public int CodigoAdministrador { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Ciudad { get; set; }
+
+    }
+}
