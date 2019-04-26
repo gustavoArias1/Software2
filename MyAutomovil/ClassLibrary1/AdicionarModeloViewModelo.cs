f1626be7062d9789d5de9381fb62863170f436d3
gustavo andres arias loaiza <guandres-94@hotmail.com>
2019-04-26 06:52:27 -0500

    Vista Adicionar Modelo

---
+++ MyAutomovil/ClassLibrary1/AdicionarModeloViewModelo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fachada
+{
+    public class AdicionarModeloViewModelo
+    {
+        public string nombreModelo { get; set; }
+        public string nombreMarca { get; set; }
+        public int  numeroPuertas { get; set; }
+        public string transmision { get; set; }
+        public string cilindraje { get; set; }
+
+    }
+}
