3debfe618a0b7a4eb1c8194a2483fc56cfb56f1f
gustavo andres arias loaiza <guandres-94@hotmail.com>
2019-04-26 03:51:26 -0500

    adicionar marca

---
+++ MyAutomovil/ClassLibrary1/AdicionarMarcaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fachada
+{
+    public class AdicionarMarcaViewModel
+    {
+        public string nombreMarca { get; set; }
+        public string pais { get; set; }
+
+    }
+}
